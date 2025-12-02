9c1e6fe3fb9cd49f9f89a7da6b89e4db36f2129a
Helektra <960421sean@gmail.com>
2025-12-02 15:58:43 +0800

    1202 - 1

--- vectorOp Basic.c
+++ vectorOp Basic.c
@@ -2,6 +2,14 @@
 #include"vectorOp.h"
 
 int* vADD(int* A,int* B,int size){
+    int* C = malloc(size* (sizeof(int) * size));
+    int i = 0;
+    for(i = 0;i < size;i ++){
+        
+        C[i] = A[i] + B[i];
+
+    }
+
     return C;
 }
 
