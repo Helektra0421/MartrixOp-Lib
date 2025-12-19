19fe714d2d636c4866e85d4265ef72668a9c20c5
Helektra <960421sean@gmail.com>
2025-12-19 22:12:40 +0800

    1219 - 2

--- main.c
+++ main.c
@@ -1,29 +1,67 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include"vectorOp.h"
 
 int main(){
-    int A[3][3] = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
-    };
-    int B[3][3] = {
-        {9, 8, 7},
-        {6, 5, 4},
-        {3, 2, 1}
-    };   
-
-    int** C;
-
-    C = vADD(&A,&B,SIZE);
-
-    int i,j = 0;
+ 	int **a;
+	int **b;
+    int **c;
     int size = SIZE;
+    int i,j = 0;
+	
+	a = (int **)malloc(3 * sizeof(int *));
+	for(i = 0;i < 3;i ++){
+		a[i] = (int *)malloc(3 * sizeof(int));
+	}
+	
+	b = (int **)malloc(3 * sizeof(int *));
+	for(i = 0;i < 3;i ++){
+		b[i] = (int *)malloc(3 * sizeof(int));
+	}
+
+	for(i = 0;i < 3;i ++){
+		for(j = 0;j < 3;j ++){
+			
+			printf("a%d%d = ?\n",i+1,j+1);
+			scanf("%d",&a[j][i]);
+			
+		}
+	}
+	
+	for(i = 0;i < 3;i ++){
+		for(j = 0;j < 3;j ++){
+
+			printf("b%d%d = ?\n",i+1,j+1);
+			scanf("%d",&b[j][i]);
+			
+		}
+	}
+	
+	printf("matrix A =\n");
+	for(i = 0;i < 3;i ++){
+		for(j = 0;j < 3;j ++){
+			printf("%d ",*(*(a+j)+i));
+		}
+		puts("");
+	}
+	puts("");
+	
+	printf("matrix B =\n");
+	for(i = 0;i < 3;i ++){
+		for(j = 0;j < 3;j ++){
+			printf("%d ",*(*(b+j)+i));
+		}
+		puts("");
+	}
+	puts("");
+
+    c = vADD(a,b,SIZE);
+
  	printf("matrix C =\n");
 	for(i = 0;i < 3;i ++){
 		for(j = 0;j < 3;j ++){
 
-			printf("%d ",*(*(C+j)+i));
+			printf("%d ",*(*(c+j)+i));
 		}
 		puts("");
 	}
